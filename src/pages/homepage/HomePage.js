ca76310af82602ff61ea8c74b240addcb267195e
jolarti <jolarti@gmail.com>
2021-12-14 17:13:48 +0100

    add BrowserRouter to index

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+function HomePage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/postpage/PostPage.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+function PostPage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default PostPage;
