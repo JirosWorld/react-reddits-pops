4953bb8095a4440c54d8c5d36c2292267d256170
jolarti <jolarti@gmail.com>
2021-12-14 16:37:57 +0100

    add react-router-dom 5.2.0

--- src/App.js
+++ src/App.js
@@ -4,18 +4,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [posts, setPosts] = useState([]);
+    const [specificPost, setSpecificPost] = useState({});
 
-  //mounting fase
+    //mounting fase
+    useEffect(() => {
+        //de functie om data op te halen
+        async function fetchPosts() {
+            try {
+                // await request:
+                const result = await axios.get('https://www.reddit.com/hot.json?limit=15');
+                console.log(result.data.data.children);
+// sla de array van posts op
+                setPosts(result.data.data.children);
+// sla echt alleen de allereerste post op
+                setSpecificPost(result.data.data.children[0]);
+            } catch (e) {
+                console.error(e);
+            }
+
+        }
+
+        fetchPosts();
+
+    }, []);
 
 
+    return (
+        <div className="App">
+            <h1>Homepagina</h1>
+            <div className="yellow">
 
-  return (
-    <div className="App">
+
+                {posts.map((post) => {
+                    // console.log(post.data);
+                    return <div>{post.data.upvote_ratio}</div>
+                })}
+            </div>
+            ;
 
 
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+            <img src={logo} className="App-logo" alt="logo"/>
+        </div>
+    )
+        ;
 }
 
 export default App;
